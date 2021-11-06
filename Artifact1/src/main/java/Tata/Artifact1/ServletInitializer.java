4ece14c6e27caf55f301fa68b4ca53b13b1a43d6
rakesh <raakhys@gmail.com>
2021-11-06 16:38:03 +0530

    last commit

---
+++ Artifact1/src/main/java/Tata/Artifact1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Tata.Artifact1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Artifact1Application.class);
+	}
+
+}
---
+++ Artifact1/src/test/java/Tata/Artifact1/Artifact1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Tata.Artifact1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Artifact1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
